93cca50532747c36f4da093480e6c7e42badfcf1
wzy9012 <457627993@qq.com>
2019-02-18 20:24:18 +0800

    123

---
+++ 01.py
@@ -0,0 +1,26 @@
+'''
+定义一个学生类
+'''
+#定义一个空的类
+class Student():
+    pass
+#定义一个对象
+mingyue = Student()
+
+#定义一个类，用来描述Python的学生
+class PythonStudent():
+    name = None
+    age = 18
+    course = "Python"
+
+
+    def doHomework(self):
+        print("I am doing honework")
+
+        return None
+
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.age)
+
+yueyue.doHomework()
